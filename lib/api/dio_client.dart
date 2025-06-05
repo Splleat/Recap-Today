1f0619bdce1e1c598d61b7dc1a663f78b342c616
SangMin Park <bak575755@gmail.com>
2025-06-06 00:53:29 +0900

    서버 url 추가

--- lib/api/dio_client.dart
+++ lib/api/dio_client.dart
@@ -4,7 +4,8 @@ class DioClient {
   static final Dio dio = Dio(
     BaseOptions(
       // baseUrl: 'http://127.0.0.1:3000', // 에뮬레이터면 10.0.2.2
-      baseUrl: 'http://10.0.2.2:3000',
+      // baseUrl: 'http://10.0.2.2:3000',
+      baseUrl: 'http://3.37.238.133:80', // 실제 서버 주소
       connectTimeout: const Duration(seconds: 100),
       receiveTimeout: const Duration(seconds: 100),
       contentType: 'application/json',
